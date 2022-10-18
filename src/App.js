53de6280a9eead8b9a75f238844584b72602d18f
1122coder <malikmuhammadkashif414@gmail.com>
2022-10-19 01:48:58 +0500

    created react app and added homepage files of jsx and scss or scass

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Homepage from './Homepage.component.jsx';
+
+
+function App() {
+  return (
+    <div>
+    <Homepage />
+    </div>
+  );
+}
+
+export default App;
